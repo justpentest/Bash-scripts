68f89da3ac3028117e08b17dc416ac3b0a0633c2
justpentest <transform2secure@gmail.com>
2016-11-07 16:42:21 +0530

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -9,3 +9,7 @@ echo $j
  for url in $(cat intel.txt);do host $url | grep "has address" | grep "intel"; done
 
 cat index.html | grep "href=" | cut -d "/" -f3 |grep "intel.com" | cut -d '"' -f1
+
+
+for url in $(cat raw_domain.txt); do host $url | grep "has address" | grep "twitter" | sort -u | cut -d " " -f1 | sort -u > temp ; done
+
